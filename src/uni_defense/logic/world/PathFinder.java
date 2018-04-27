db475173ae60d67f18b7ff1c60c4151b7cf7e84b
Rafael Rego Drumond <radrumond@gmail.com>
2018-04-27 16:41:47 +0200

    updates, non working pathfinder

---
+++ src/uni_defense/logic/world/PathFinder.java
@@ -0,0 +1,25 @@
+package uni_defense.logic.world;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PathFinder {
+	private World world;
+	public PathFinder(World world) {
+		this.world=world;
+	}
+	
+	
+	
+	public List<Point> findPath(int x1, int y1, int x2, int y2) {
+		List<Point> a = new LinkedList<>();
+		
+		
+		
+		
+		
+		return null;
+	}
+	
+	
+}
---
+++ test/uni_defense/PathFindingTest.java
@@ -0,0 +1,13 @@
+package uni_defense;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PathFindingTest {
+	
+	@Test
+	public void test() {
+		assertEquals(4, 5);
+	}
+}
