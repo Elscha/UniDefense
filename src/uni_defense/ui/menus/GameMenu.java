2a5049053475babe122aa7c9998c1f4142197b4b
MaximilianNeu <maximilian.neubarth@gmail.com>
2018-04-28 00:12:19 +0200

    Player and GameMenu updated and now related

--- src/uni_defense/ui/menus/GameMenu.java
+++ src/uni_defense/ui/menus/GameMenu.java
@@ -6,16 +6,10 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
+import uni_defense.logic.player.Player;
+
 public class GameMenu extends JToolBar {
 
-	private int gold;
-	private int currentlifes;
-	private int maxlifes;
-	private int currentwave;
-	private int endwave;
-	private int enemiesKilled;
-	private int enemiesAlive;
-	
 	public GameMenu() {
 		updateMenubar();
 		this.setFloatable(false);
@@ -24,31 +18,31 @@ public class GameMenu extends JToolBar {
 
 	private void updateMenubar() {
 		
-		JTextField jtextGold = new JTextField(("Gold : " + gold));
+		JTextField jtextGold = new JTextField(("Gold : " + Player.INSTANCE.getGold()));
 		jtextGold.setColumns(10);
 		jtextGold.setEnabled(false);
         this.add(jtextGold);
         this.addSeparator();        
 
-		JTextField jtextLifes = new JTextField(("Lifes : " + currentlifes + "/" + maxlifes));
+		JTextField jtextLifes = new JTextField(("Lifes : " + Player.INSTANCE.getCurrentlifes() + "/" + Player.INSTANCE.getMaxlifes()));
 		jtextLifes.setColumns(10);
 		jtextLifes.setEnabled(false);
         this.add(jtextLifes);
         this.addSeparator();        
         
-		JTextField jtextWave = new JTextField(("Wave : " + currentwave + "/" + endwave));
+		JTextField jtextWave = new JTextField(("Wave : " + Player.INSTANCE.getCurrentwave() + "/" + Player.INSTANCE.getEndwave()));
 		jtextWave.setColumns(10);
 		jtextWave.setEnabled(false);
         this.add(jtextWave);
         this.addSeparator();        
         
-		JTextField jtextKills = new JTextField(("Total killcount : " + enemiesKilled));
+		JTextField jtextKills = new JTextField(("Total killcount : " + Player.INSTANCE.getEnemiesKilled()));
 		jtextKills.setColumns(12);
 		jtextKills.setEnabled(false);
         this.add(jtextKills);
         this.addSeparator();        
 
-		JTextField jtextEnemies = new JTextField(("Enemies alive : " + enemiesAlive));
+		JTextField jtextEnemies = new JTextField(("Enemies alive : " + Player.INSTANCE.getEnemiesAlive()));
 		jtextEnemies.setColumns(10);
 		jtextEnemies.setEnabled(false);
         this.add(jtextEnemies);
@@ -63,53 +57,5 @@ public class GameMenu extends JToolBar {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-	}
-	
-	public void updateGold(int goldChange) {
-		gold = gold + goldChange;
-	}
-	
-	public void setGold(int gold) {
-		this.gold = gold;
-	}
-	
-	public void updateCurrentlifes(int lifesChange) {
-		currentlifes = currentlifes + lifesChange;
-	}
-	
-	public void setCurrentlifes(int currentlifes) {
-		this.currentlifes = currentlifes;
-	}
-	
-	public void setMaxlifes(int maxlifes) {
-		this.maxlifes = maxlifes;
-	}
-	
-	public void increaseWaveByOne() {
-		currentwave++;
-	}
-	
-	public void setCurrentwave(int currentwave) {
-		this.currentwave = currentwave;
-	}
-	
-	public void setEndwave(int endwave) {
-		this.endwave = endwave;
-	}
-	
-	public void increaseEnemiesKilledByOne() {
-		enemiesKilled++;
-	}
-	
-	public void setEnemiesKilled(int enemiesKilled) {
-		this.enemiesKilled = enemiesKilled;
-	}
-	
-	public void decreaseEnemiesAliveByOne() {
-		enemiesAlive--;
-	}
-	
-	public void setEnemiesAlive(int enemiesAlive) {
-		this.enemiesAlive = enemiesAlive;
 	}
 }
